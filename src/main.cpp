a1362d32084166d46e09888befce970f36f08047
Brandon Fogerty <bfogerty@gmail.com>
2017-01-14 22:49:26 -0800

    Added a Renderer

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,13 @@
 #include <math.h>
 #include <algorithm>
 #include <thread>
+#include "renderer/renderer.h"
+#include "math/CommonMath.h"
 #include "math/vec2.h"
 #include "color/color.h"
 #include "time/StopWatch.h"
 #include "export/PPMExporter.h"
 using namespace std;
-
-
-float clamp(float value, float min, float max)
-{
-	if (value < min)
-	{
-		value = min;
-	}
-	else if (value > max)
-	{
-		value = max;
-	}
-
-	return value;
-}
-
-float clamp01(float value)
-{
-	return clamp(value, 0.000f, 1.0f);
-}
-
-int jobsCompleted = 0;
 
 void kernel(	const unsigned int& x, 
 				const unsigned int& y, 
@@ -42,88 +22,15 @@ void kernel(	const unsigned int& x,
 
 	if (abs(uv.X()) < 0.0000f)
 	{
-		uv.X() = 0.0001f;
+		uv.X() = 0.0000f;
 	}
 
 	float l = uv.Magnitude();
 
-	float t = float(pow(abs(1.0f / (uv.X() * 50.0)), 0.5));
+	float t = float(pow(abs(1.0f / (((uv.X() * 300.0) + sin(uv.Y() * 5.0f)*30.0f))), 0.7f));
 	t = clamp01(t);
 	outputColor.SetValues(t * 2.0f, t * 4.0f, t * 8.0f, 1.0f);
 	Color::Clamp(outputColor, 0.0f, 1.0f);
-}
-
-void Scene_Thread(int startingX, int startingY, int endingX, int endingY, int width, int height, int bytesPerPixel, float* pixels)
-{
-	Color outputColor;
-	for (int y = startingY; y < endingY; ++y)
-	{
-		for (int x = startingX; x < endingX;  ++x)
-		{
-			kernel(x, y, width, height, outputColor);
-
-			int redIndex = (y*width*bytesPerPixel) + (x*bytesPerPixel + 0);
-			memcpy(&pixels[redIndex], outputColor.GetValues(), sizeof(float) * 3);
-		}
-	}
-
-	++jobsCompleted;
-}
-
-void Scene_Threads(int width, int height, int bytesPerPixel, float* pixels)
-{
-
-	int d = width / 5;
-
-	std::thread t1(Scene_Thread, 0, 0, d, height, width, height, bytesPerPixel, pixels);
-	t1.detach();
-
-	std::thread t2(Scene_Thread, d, 0, d*2, height, width, height, bytesPerPixel, pixels);
-	t2.detach();
-
-	std::thread t3(Scene_Thread, d*2, 0, d*3, height, width, height, bytesPerPixel, pixels);
-	t3.detach();
-
-	std::thread t4(Scene_Thread, d * 3, 0, d * 4, height, width, height, bytesPerPixel, pixels);
-	t4.detach();
-
-	std::thread t5(Scene_Thread, d * 4, 0, width, height, width, height, bytesPerPixel, pixels);
-	t5.detach();
-
-	while (jobsCompleted < 5);
-}
-
-void Scene_NoThreads(int width, int height, int bytesPerPixel, float* pixels)
-{
-	float aspectRatio = (float)width / height;
-
-	for(int y = 0; y < height; ++y)
-	{
-		for(int x = 0; x < width; ++x)
-		{
-			int redIndex	= (y*width*bytesPerPixel) + (x*bytesPerPixel + 0);
-			int greenIndex	= (y*width*bytesPerPixel) + (x*bytesPerPixel + 1);
-			int blueIndex	= (y*width*bytesPerPixel) + (x*bytesPerPixel + 2);
-
-			float uvx = (float(x) / width) * 2.0f - 1.0f;
-			float uvy = (float(y) / height) * 2.0f - 1.0f;
-			uvx *= aspectRatio;
-
-			if( abs(uvx) < 0.0000f )
-			{
-				uvx = 0.0001f;
-			}
-
-			float l = sqrt(uvx*uvx + uvy*uvy);
-
-			float t = float(pow(abs( 1.0f / (l * 50.0)), 0.5));
-			t = clamp01(t);
-
-			pixels[redIndex]	= clamp01(t * 2.0f);
-			pixels[greenIndex]	= clamp01(t * 4.0f);
-			pixels[blueIndex]	= clamp01(t * 8.0f);
-		}
-	}
 }
 
 int main(int argc, char* argv)
@@ -133,14 +40,15 @@ int main(int argc, char* argv)
 	const int width = 320;
 	const int height = 240;
 	const int bytesPerPixel = 3;
-	float pixels[width * height * bytesPerPixel];
 	
+	Renderer renderer(kernel, width, height, bytesPerPixel);
+
 	StopWatch timer;
 	timer.Start();
-	Scene_Threads(width, height, bytesPerPixel, pixels);
+	renderer.Render();
 	float elapsedTimeInMS = timer.Stop();
 
-	PPMExporter::Export( "test2.ppm", width, height, bytesPerPixel, pixels);
+	PPMExporter::Export( "test2.ppm", width, height, bytesPerPixel, renderer.GetPixels());
 
 	cout << "-- Ray Tracer End \n";
 	cout << "STATS\n";
